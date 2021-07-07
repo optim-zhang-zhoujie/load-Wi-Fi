c0d67204b388fc31f9ce13356ff646311a5634b7
optim-zhang-zhoujie <77651263+optim-zhang-zhoujie@users.noreply.github.com>
2021-07-07 11:14:42 +0900

    for 20.04

--- load-wifi.sh
+++ load-wifi.sh
@@ -1,13 +1,33 @@
 #!/bin/bash
-touch ~/setup_wifi
+touch ~/setup_wifi.sh
 
-cat <<EOT >> ~/setup_wifi
+cat <<EOT >> ~/setup_wifi.sh
 #!/bin/bash
 sudo modprobe rtl8192cu
 echo "056E 4009" | sudo tee /sys/bus/usb/drivers/rtl8192cu/new_id
 EOT
 
-chmod 755 ~/setup_wifi
+chmod 744 ~/setup_wifi.sh
 
-sudo mv ~/setup_wifi /etc/init.d/setup_wifi
-sudo update-rc.d setup_wifi defaults 90
+sudo mv ~/setup_wifi.sh /usr/local/bin/setup_wifi.sh
+
+touch ~/setup_wifi.service
+
+cat <<EOT >> ~/setup_wifi.service
+[Unit]
+Description=Setup Wi-Fi driver
+After=syslog.target network.target
+
+[Service]
+ExecStart=/usr/local/bin/setup_wifi.sh
+
+[Install]
+WantedBy=default.target
+EOT
+
+chmod 664 ~/setup_wifi.service
+
+sudo mv ~/setup_wifi.service /etc/systemd/system/setup_wifi.service
+
+sudo systemctl daemon-reload
+sudo systemctl enable disk-space-check.service
